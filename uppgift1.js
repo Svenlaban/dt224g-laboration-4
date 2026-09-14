388b9e95f9f943e8a6c292469fac74397b59504e
Svenlaban <svenlaban@outlook.com>
2026-09-14 21:38:45 +0200

    Uppgift 1 klar

--- uppgift1.js
+++ uppgift1.js
@@ -1,8 +1,13 @@
 /* Lösning till Uppgift 1. Av Micael Huczkowski, 2026 */
 /* Program som skapar variabler som representerar information om en person. */
+/* Körs enklast med "node uppgift1.js" i terminalen */
 "use strict";
 
 const firstName = "Micael";
 const lastName = "Huczkowski";
 const age = 39;
 const isStudent = true;
+
+console.log(firstName, lastName);
+console.log("Ålder:", age);
+console.log("Student:", isStudent);
