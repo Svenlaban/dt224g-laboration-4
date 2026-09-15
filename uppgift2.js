e9a993ba24c7029f77ffa135a9e66d264bdb36f5
Svenlaban <svenlaban@outlook.com>
2026-09-15 08:09:43 +0200

    Uppgift 2 klar

---
+++ uppgift2.js
@@ -0,0 +1,15 @@
+/* Lösning till Uppgift 2. Av Micael Huczkowski, 2026 */
+/* Program som skapar räknar ut totalpris på antal varor, samt lägger på moms */
+/* Körs enklast med "node uppgift2.js" i terminalen */
+"use strict";
+
+const cost = 100;
+const quantity = 3;
+
+const totalCost = cost * quantity;
+const withTax = totalCost * 1.25;
+
+console.log("Pris:", cost, "kr");
+console.log("Antal:", quantity);
+console.log("Totalt:", totalCost, "kr");
+console.log("Totalt inkulsive moms:", withTax, "kr");
