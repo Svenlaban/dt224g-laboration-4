6bf7158a19fe23bd780cef696b30402af743c810
Svenlaban <svenlaban@outlook.com>
2026-09-15 22:59:20 +0200

    Editerade kommentar

--- uppgift4.js
+++ uppgift4.js
@@ -1,5 +1,5 @@
 /* Lösning till Uppgift 4. Av Micael Huczkowski, 2026 */
-/* Program som skriver ut alla heltal från 1 till 20*/
+/* Program som skriver ut alla jämna heltal från 1 till 20*/
 /* Körs enklast med "node uppgift4.js" i terminalen */
 "use strict";
 
