be1dadb9b1bf282c78584de2f3fcf398651dddee
Svenlaban <svenlaban@outlook.com>
2026-09-16 09:59:14 +0200

    Uppgift5 klar

--- uppgift5.js
+++ uppgift5.js
@@ -1,5 +1,5 @@
 /* Lösning till Uppgift 5. Av Micael Huczkowski, 2026 */
-/* Program som skapar variabler som representerar information om en person. */
+/* Program som skriverut maträtter från en array. */
 /* Körs enklast med "node uppgift5.js" i terminalen */
 "use strict";
 
@@ -10,5 +10,20 @@ const food = [
     "Sushi",
     "Pavlova"
 ];
-
+/* skriver ut hela arrayen */
 console.log("Hela arrayen:", food);
+
+/* skriver ut första elementet*/
+console.log("Första elemntet:", food[0]);
+
+/* skriver ut sista elementet */
+console.log("Sista elemntet:", food[food.length - 1]);
+
+/* lägger till en maträtt */
+food.push("Köttbullar");
+
+/* tar bort första maträtten */
+food.shift();
+
+/* Skriver ut arrayen igen för att visa förändringar */
+console.log("Hela arrayen efter ändringar:", food);
