eacabe48c4bc1edb66ca2761fd65420b5b4fe65b
Svenlaban <svenlaban@outlook.com>
2026-09-16 08:26:36 +0200

    Uppgift 5, skriver helaarrayen

---
+++ uppgift5.js
@@ -0,0 +1,14 @@
+/* Lösning till Uppgift 5. Av Micael Huczkowski, 2026 */
+/* Program som skapar variabler som representerar information om en person. */
+/* Körs enklast med "node uppgift5.js" i terminalen */
+"use strict";
+
+const food = [
+    "Pizza",
+    "Hamburgare",
+    "Pannkaka",
+    "Sushi",
+    "Pavlova"
+];
+
+console.log("Hela arrayen:", food);
