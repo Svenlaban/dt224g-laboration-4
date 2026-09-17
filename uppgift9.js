b006a348fc74483e2c33cf1f5f005d6d714c5e74
Micael Huczkowski <svenlaban@outlook.com>
2026-09-17 07:34:34 +0200

    Uppgift 8 skapat array

---
+++ uppgift9.js
@@ -0,0 +1,22 @@
+/* Lösning till Uppgift 9. Av Micael Huczkowski, 2026 */
+/* Program som går igenom en array med personupgifter och avgör om personer är myndiga eller inte */
+/* Körs enklast med "node uppgift9.js" i terminalen */
+"use strict";
+
+const people = [
+  {
+    name: "Pippi Långstrump",
+    age: 9,
+    city: "Visby"
+  },
+  {
+    name: "Emil Svensson",
+    age: 8,
+    city: "Lönneberga"
+  },
+  {
+    name: "91:an Karlsson",
+    age: 20,
+    city: "Klackamo"
+  }
+];
